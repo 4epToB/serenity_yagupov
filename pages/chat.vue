e7c42fdcf3fc5b00171d3d4da9091f91e249a060
4epToB <73242484+4epToB@users.noreply.github.com>
2021-03-18 16:47:38 +0300

    main

---
+++ pages/chat.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mainwrap">
+      <div class="chatwrap">
+          <div class="textarea">
+            <b-list-group>
+                <b-list-group-item variant="light" v-for="(message,index) in messages"
+                :key="index"
+                >
+                  {{message.time}}|
+                  <span style="font-weight: 900">{{message.username}}</span>: 
+                  <span >{{message.text}}</span>
+                </b-list-group-item>
+            </b-list-group>   
+          </div>
+          <div class="input">
+            <b-form-input 
+              type="text" 
+              id="myInput" 
+              v-model="textMessage" 
+              autofocus 
+              ></b-form-input>
+            <b-button variant="dark" id="myBtn" @click="sendMessage">Отправить</b-button>
+          </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+        username:"",
+        visible:false,
+        messages:[],
+        textMessage:"",
+
+
+    }
+  },
+  methods:{
+  },
+  computed:{
+
+  },
+  updated: function () {
+    this.$nextTick(function () {
+      var myDiv = document.querySelector(".textarea")
+      myDiv.scrollTop = myDiv.scrollHeight 
+    })
+  },
+  mounted: function () {
+    },
+
+}
+</script>
+
+<style>
+    .mainwrap{
+      padding: 10px;
+      display: flex;
+      height: calc(100vh - 340px);
+      justify-content: space-between;
+        
+    }
+    .chatwrap{
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      flex-grow: 1;
+    }
+    .textarea{
+      overflow: auto; 
+      margin-bottom: 10px;
+      flex-grow: 1;
+      padding-bottom:10px ;
+
+    }
+    .input{
+      display: flex;
+      justify-content: space-between;
+    }
+    .input input{
+      flex-grow: 0.99;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="reg" v-if="!visible">
+      <div class="line">
+        <label for="text-name">Имя</label>
+        <b-form-input v-model="username" id="text-name" placeholder="Введите Ваше имя" ></b-form-input>
+      </div>
+      <div class="buttonwrap">
+        <b-button @click="login" variant="success">Вход</b-button>
+      </div>
+
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+        username:"",
+        visible:false
+
+    }
+  },
+  methods:{
+    login(){
+          this.visible=!this.visible
+          this.$router.push('/chat')
+    },
+  }
+
+}
+</script>
+
+<style>
+  .reg{
+    width: 250px;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border:2px solid #6a89cc;
+    background-color:#38ada9 ;
+    border-radius:20px;
+    padding:20px;
+    margin:150px auto
+  }
+  .line{
+    width: 206px;
+    height: 70px;
+  }
+  .line input{
+    width: 206px;
+    height: 30px;
+  }
+  .buttonwrap{
+    height: 40px;
+    display: flex;
+    justify-content: center;
+  }
+</style>
