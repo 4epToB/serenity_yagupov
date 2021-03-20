ccaca7a3c6fe85441b582a440878d9444fb5e8d9
4epToB <73242484+4epToB@users.noreply.github.com>
2021-03-20 12:47:39 +0300

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -6,18 +6,17 @@
                 <b-list-group-item variant="light" v-for="(message,index) in messages"
                 :key="index"
                 >
-                  {{message.time}}|
-                  <span style="font-weight: 900">{{message.username}}</span>: 
+                  <span style="font-weight: 900">{{message.username}}</span>:
                   <span >{{message.message}}</span>
                 </b-list-group-item>
-            </b-list-group>   
+            </b-list-group>
           </div>
           <div class="input">
-            <b-form-input 
-              type="text" 
-              id="myInput" 
-              v-model="message" 
-              autofocus 
+            <b-form-input
+              type="text"
+              id="myInput"
+              v-model="message"
+              autofocus
               ></b-form-input>
             <b-button variant="dark" id="myBtn" @click="send">Отправить</b-button>
           </div>
@@ -42,14 +41,14 @@ export default {
       const requestOptions = {
         headers: { "Content-Type": "application/json" },
       };
-      const response = await this.$axios.post('/my-api/', JSON.stringify({ username: this.username ,message : this.message}),requestOptions)
+      const response = await this.$axios.post('/messages', JSON.stringify({ username: this.username ,message : this.message}),requestOptions)
       this.message=''
       console.log(response.data)
       /* this.messages.push(response.data) */
 
     },
     async refresh(){
-      const messages = await this.$axios.$get('/my-api/')
+      const messages = await this.$axios.$get('/messages')
       this.messages = messages
     }
   },
@@ -59,7 +58,7 @@ export default {
   updated() {
     this.$nextTick(function () {
       var myDiv = document.querySelector(".textarea")
-      myDiv.scrollTop = myDiv.scrollHeight 
+      myDiv.scrollTop = myDiv.scrollHeight
     })
   },
   created() {
@@ -69,9 +68,9 @@ export default {
     setInterval(function () {
 			this.refresh()
 		}.bind(this), 1500)
-   
-    
-    
+
+
+
   },
   mounted(){
     var input = document.getElementById("myInput");
@@ -84,7 +83,6 @@ export default {
   }
 
 }
-
 </script>
 
 <style>
@@ -93,7 +91,7 @@ export default {
       display: flex;
       height: calc(100vh - 340px);
       justify-content: space-between;
-        
+
     }
     .chatwrap{
       display: flex;
@@ -102,7 +100,7 @@ export default {
       flex-grow: 1;
     }
     .textarea{
-      overflow: auto; 
+      overflow: auto;
       margin-bottom: 10px;
       flex-grow: 1;
       padding-bottom:10px ;
