91d9ce2866f7a94f709d339a4200bd7b89b37c99
4epToB <73242484+4epToB@users.noreply.github.com>
2021-03-20 11:17:05 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
     htmlAttrs: {
       lang: 'ru'
     },
+    telemetry: false,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
